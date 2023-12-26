ae2f2807180e8ff800ada94d3814f11b8f3e5fe4
Hashim Qureshi C <qureshi@Hashims-MacBook-Air.local>
2023-12-27 00:55:21 +0530

    first

---
+++ src/Sample1/Index2.java
@@ -0,0 +1,11 @@
+package Sample1;
+
+public class Index2 {
+    static int x=10;
+    int y =20;
+
+    public static void main(String[] args) {
+        Index2 i2 =new Index2();
+        System.out.println(x+i2.y);
+    }
+}
