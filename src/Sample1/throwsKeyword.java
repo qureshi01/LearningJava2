bd379f04b98cc3c9ff9c50554286ea762af7ace8
Hashim Qureshi Chennadan <142244206+qureshi01@users.noreply.github.com>
2024-01-06 14:44:34 +0530

    intial

--- src/Sample1/throwsKeyword.java
+++ src/Sample1/throwsKeyword.java
@@ -2,25 +2,34 @@ package Sample1;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.InputMismatchException;
 
 public class throwsKeyword {
     public static void main(String[] args) throws IOException {
         int x=10;
-        int y=0;
-
+        int y=getFromDB();
+        int z=0;
         try {
-            int z = x / y;
-            fun();
+            z = x / y;
+
         }catch(ArithmeticException ex){
             System.out.println("ArithmeticError");
-        }catch (IOException ex){
-            System.out.println("FileNotFound");
-        }
-
+        }//catch (IOException ex){
+        //   System.out.println("FileNotFound");
+        //}
+        fun(z);
         System.out.println("Done");
     }
 
-    public static void fun() throws IOException{
-           PrintWriter pw = new PrintWriter("abc1.txt");
+    private static int getFromDB() {
+        return 0;
+    }
+
+    public static void fun(int z){ //throws IOException{
+        if(z<=0){
+            throw new InputMismatchException("Invalid Input");
+        }
+          // PrintWriter pw = new PrintWriter("abc1.txt");
+          // pw.println(z);
     }
 }
