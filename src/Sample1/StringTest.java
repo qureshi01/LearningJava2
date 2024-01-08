aa2386460d83d225b9309984de755abb2432b4bb
Hashim Qureshi Chennadan <142244206+qureshi01@users.noreply.github.com>
2024-01-08 21:08:17 +0530

    intial

---
+++ src/Sample1/StringTest.java
@@ -0,0 +1,12 @@
+package Sample1;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String str = "10";
+        String str1 = "10.34";
+        int val = Integer.parseInt(str);
+        float val1 = Float.parseFloat(str1);
+        System.out.println(val);
+        System.out.println(val1);
+    }
+}
