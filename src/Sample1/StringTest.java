d7303fc84b1141969c46f2e16f1ae1b07c7ce4be
Hashim Qureshi Chennadan <142244206+qureshi01@users.noreply.github.com>
2024-01-08 21:10:24 +0530

    intial

--- src/Sample1/StringTest.java
+++ src/Sample1/StringTest.java
@@ -2,11 +2,13 @@ package Sample1;
 
 public class StringTest {
     public static void main(String[] args) {
+        String str2 = new String("Done");
         String str = "10";
         String str1 = "10.34";
         int val = Integer.parseInt(str);
         float val1 = Float.parseFloat(str1);
         System.out.println(val);
         System.out.println(val1);
+        System.out.println(str2);
     }
 }
